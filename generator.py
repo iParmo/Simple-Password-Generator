6c91c19fc11d32ee4ea4923f43024f33bd08c6df
Magnus Parmo <73383707+iParmo@users.noreply.github.com>
2020-11-08 17:14:18 +0100

    Added length changer and other small changes
    You can now give the length you want your password to be. If invalid input is given start over automatically

--- generator.py
+++ generator.py
@@ -13,11 +13,19 @@ def generator(length=8):
     printable = list(printable)
     random.shuffle(printable)
 
+
+    try:
+        chars = int(input("How many characters do you want your password to be?: "))
+        length = chars or length
+    except ValueError:
+        pass
+
     random_password = random.choices(printable, k=length)
     random_password = ''.join(random_password)
+
+
     pyperclip.copy(random_password)
     print(f'{random_password} Has been copied to your clipboard!')
-
     yes = input("Do you want to log generated password to log.txt? (y/n): ")
     if yes == 'y':
         print(f'{random_password} Has been logged to the logs.txt file!')
@@ -27,7 +35,8 @@ def generator(length=8):
     elif yes == 'n':
         print("Your password haven't been logged!")
     else:
-        print("No invalid input please try again.")
+        print("Invalid input please try again.")
+        generator()
 
 generator()
 # Length will default to 8 if nothing is put in the call ()

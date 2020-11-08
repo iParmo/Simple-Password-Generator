c00861df78498427cf80c007499aba9215c17b84
Magnus Parmo <73383707+iParmo@users.noreply.github.com>
2020-11-08 16:35:46 +0100

    Made alot of useful changes!
    Added automatic copy to clipboard and optional log system!

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 import string
 import random
+import pyperclip
 
 LETTERS = string.ascii_letters
 NUMBERS = string.digits
@@ -14,6 +15,19 @@ def generator(length=8):
 
     random_password = random.choices(printable, k=length)
     random_password = ''.join(random_password)
-    print(random_password)
+    pyperclip.copy(random_password)
+    print(f'{random_password} Has been copied to your clipboard!')
 
-generator() # Length will default to 8 if nothing is put in the call ()
+    yes = input("Do you want to log generated password to log.txt? (y/n): ")
+    if yes == 'y':
+        print(f'{random_password} Has been logged to the logs.txt file!')
+        log = open("logs.txt", "a")
+        log.write(f'{random_password}\n')
+        log.close()
+    elif yes == 'n':
+        print("Your password haven't been logged!")
+    else:
+        print("No invalid input please try again.")
+
+generator()
+# Length will default to 8 if nothing is put in the call ()

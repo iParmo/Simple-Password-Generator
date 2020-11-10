57615e0973747f5939174137400b5df6780a9248
Magnus Parmo <73383707+iParmo@users.noreply.github.com>
2020-11-10 22:55:41 +0100

    Made the code look Cleaner!

--- generator.py
+++ generator.py
@@ -7,36 +7,43 @@ NUMBERS = string.digits
 PUNCTUATION = string.punctuation
 
 
-def generator(length=8):
+def generator(length=8, save_to_file='y'):
     printable = f'{LETTERS}{NUMBERS}{PUNCTUATION}'
 
     printable = list(printable)
     random.shuffle(printable)
 
-
-    try:
-        chars = int(input("How many characters do you want your password to be?: "))
-        length = chars or length
-    except ValueError:
-        generator()
-
     random_password = random.choices(printable, k=length)
     random_password = ''.join(random_password)
 
-
     pyperclip.copy(random_password)
     print(f'{random_password} Has been copied to your clipboard!')
-    yes = input("Do you want to log generated password to log.txt? (y/n): ")
-    if yes == 'y':
+    if save_to_file == 'y':
         print(f'{random_password} Has been logged to the logs.txt file!')
         log = open("logs.txt", "a")
         log.write(f'{random_password}\n')
         log.close()
-    elif yes == 'n':
+    elif save_to_file == 'n':
         print("Your password haven't been logged!")
     else:
-        print("Invalid input please try again.")
-        generator()
+        print("I don't know what you're trying to do.")
 
-generator()
-# Length will default to 8 if nothing is put in the call ()
+
+def user_input(length=8):
+    try:
+        chars = int(input("How many characters do you want your password to be?: "))
+        length = chars or length
+    except ValueError:
+        user_input(length=length)
+
+    save_to_file = input("Do you want to log generated password to log.txt? (y/n): ")
+
+    if not save_to_file == 'y' and not save_to_file == 'n':
+        print("Invalid input please try again.")
+        user_input(length=length)
+
+    return length, save_to_file
+
+
+length, save_to_file = user_input()
+generator(length, save_to_file)
